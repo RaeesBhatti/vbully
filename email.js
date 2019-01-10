f3c422e00945ee0ac8e2e61d125b07b788cf2ab5
Raees Iqbal <raees.bhatti@gmail.com>
2019-01-11 02:06:17 +0500

    Move normalization up

--- email.js
+++ email.js
@@ -12,9 +12,9 @@ Object.entries(data['info']).map(([key, obj]) => {
     if (obj.contact) {
         userEmails = userEmails.concat(obj.contact.match(/[\\t|\\n| ]?([a-zA-Z0-9\._-]+\@[a-zA-Z]+\.(com|net))/g));
     }
-    userEmails = userEmails.filter(i => !!i && !i.endsWith('facebook.com'));
+    userEmails = userEmails.filter(i => !!i && !i.endsWith('facebook.com')).map(e => e.toLowerCase().trim());
     if (userEmails.length) {
-        emails[key] = Array.from(new Set(userEmails.map(e => e.toLowerCase().trim())));
+        emails[key] = Array.from(new Set(userEmails));
     }
 });
 console.log(emails, Object.keys(emails).length);
